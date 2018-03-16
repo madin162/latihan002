b6c4578879927bcb460c54e4efaf753b5622e532
reinaldyjalu <36924900+reinaldyjalu@users.noreply.github.com>
2018-03-16 09:26:02 +0700

    Create songplayed_counter.py
    This program is for counting how many times the song is played.
    The program is for lastfm dataset.
    first commit

---
+++ ReinaldyJalu/songplayed_counter.py
@@ -0,0 +1,14 @@
+from mrjob.job import MRJob
+
+class MyMapReduceJob(MRJob):
+
+	def mapper(self, _, line):
+		lines = line.split("\t")
+		traname = lines[5]
+		yield traname, 1
+			
+	def reducer(self, key, values):
+		yield key, sum(values)
+		
+if __name__ == '__main__':
+	MyMapReduceJob.run()
