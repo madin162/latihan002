627c728965f966ec49d4eb7128db6ce984431a88
rezafpratama <rezafpratama@gmail.com>
2018-04-04 16:18:32 +0700

    frekuensi 1 lagu diputar v.1

---
+++ Reza/playing.py
@@ -0,0 +1,29 @@
+from mrjob.job import MRJob
+
+
+
+class RezaPlaylist(MRJob):
+
+
+
+    def mapper(self, _, line):
+
+        data = line.split('\t')
+
+        lagu = data[5]
+
+
+
+        yield lagu, 1
+
+
+
+    def reducer(self, key, values):
+
+        yield key, sum(values)
+
+
+
+if __name__ == '__main__':
+
+    RezaPlaylist.run()
