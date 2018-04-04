0cf31a29a09ae644ee53494c2b3a7d50c44b33fa
rezafpratama <rezafpratama@gmail.com>
2018-04-04 17:01:35 +0700

    ver 1

--- Reza/playing.py
+++ Reza/playing.py
@@ -1,29 +1,10 @@
 from mrjob.job import MRJob
-
-
-
 class RezaPlaylist(MRJob):
-
-
-
     def mapper(self, _, line):
-
-        data = line.split('\t')
-
+        data = line.split("\t")
         lagu = data[5]
-
-
-
         yield lagu, 1
-
-
-
     def reducer(self, key, values):
-
         yield key, sum(values)
-
-
-
 if __name__ == '__main__':
-
     RezaPlaylist.run()
